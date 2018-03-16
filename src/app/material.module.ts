34c2346d8abcf1441462ab78b3e511062e43555b
Gabriel Rezzonico <gabrielrezzonico@gmail.com>
2018-03-16 13:33:25 -0300

    Add some content and examples to the main page. Add mat-icon.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule],
 })
 export class MaterialModule { }
